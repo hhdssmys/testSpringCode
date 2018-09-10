aab670e4c48cc010fecc902c1eced8b3dacf72d9
W3L <1174014933@qq.com>
2018-09-10 18:20:26 +0800

    Scope练习尚未补全取值说明及pom添加

---
+++ src/main/java/com/generalichina/scopetest/DemoSingletonService.java
@@ -0,0 +1,8 @@
+package com.generalichina.scopetest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoSingletonService {
+	//默认@Scope是Singleton
+}
---
+++ src/main/java/com/generalichina/scopetest/ScopeConfig.java
@@ -0,0 +1,11 @@
+package com.generalichina.scopetest;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.generalichina.scopetest")
+public class ScopeConfig {
+
+}
