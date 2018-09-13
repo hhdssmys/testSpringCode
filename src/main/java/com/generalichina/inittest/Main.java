b5834967a84944e8f7083c5081d79e75fde2870f
W3L <1174014933@qq.com>
2018-09-13 18:23:00 +0800

    注解用处说明

--- src/main/java/com/generalichina/inittest/Main.java
+++ src/main/java/com/generalichina/inittest/Main.java
@@ -11,6 +11,8 @@ public class Main {
 		 * spring负责javabean的创建与销毁,先初始化的后销毁（C++的类似于析构函数）,
 		 * bean之间的先后构造依据配置声明顺序
 		 * 该过程具有幂等性
+		 * 执行顺序：@PostConstruct可以用来在构造方法之后完成一些初始化操作,而一些初始化操作往往依赖一些注入，所以其不能在构造方法中完成
+		 * Constructor >> @Autowired >> @PostConstruct
 		 */
 		context.close();
 	}
